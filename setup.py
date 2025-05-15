1eadd2180adc1f2c90f179f441783a463524ba16
Noah Olson <21noolson@gmail.com>
2025-05-15 17:47:57 -0500

    UPdated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='agroecometrics',
-    version='0.0.2b2',
+    version='0.0.3b1',
     author='Scarlett Olson',
     author_email='Scarlett.Olson@wisc.edu',
     description='This package contains useful tools for Aggriculture and Ecological researchers to clean,' \
